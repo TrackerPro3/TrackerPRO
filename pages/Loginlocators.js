9cb39691a96988c7425f74d24505d664bc57436e
suryateja <suryatejadavuluri@gmail.com>
2020-07-24 20:03:56 +0530

    Added defaultPassword and 3 Login Scenarios

--- pages/Loginlocators.js
+++ pages/Loginlocators.js
@@ -4,17 +4,18 @@ module.exports = {
 
   // insert your locators and methods here
 
-_init() {
+  _init() {
 
   I = require('../steps_file.js')();
-},
+  },
 
   locators: {
     username: "ctl00$cphBody$txtUsername",
     password: "#ctl00_cphBody_txtPassword",
     // selectOrg: "//div[contains(text(),'QA - Surya')]",
     // selectOrg: "//div[contains(text(),'" + orgName + "')]",
-    selectButton : ".tpro-btn-icon-label"
+    selectButton : ".tpro-btn-icon-label",
+    alertContent : '.alert-contents'
 
   },
 
@@ -32,22 +33,96 @@ _init() {
    I.click(this.locators.selectButton);
  },
 
-
+// This function chooses which build to Login
  SelectBuild(BuildName) {
     let build = BuildName
     switch(build) {
 
+      case 'Bravo': I.amOnPage('https://cdrdevapp16.ryanco.com/Bravo/');
+      break;
       case 'Charlie': I.amOnPage('https://dwsapp16a.ryanco.com/Charlie/');
       break;
-
-      case 'Hotel': I.amOnPage('https://cdrdevapp16.ryanco.com/Hotel');
+      case 'Delta': I.amOnPage('https://cdrdevapp16.ryanco.com/Delta/');
+      break;
+      case 'Echo': I.amOnPage('https://dwsapp16a.ryanco.com/Echo/');
+      break;
+      case 'Foxtrot': I.amOnPage('https://dwsapp16a.ryanco.com/Foxtrot/');
+      break;
+      case 'Golf': I.amOnPage('https://cdrdevapp16.ryanco.com/Golf/');
+      break;
+      case 'Hotel': I.amOnPage('https://dwsapp16a.ryanco.com/Charlie/');
+      break;
+      case 'India': I.amOnPage('https://dwsapp16a.ryanco.com/India/');
+      break;
+      case 'Juliett': I.amOnPage('https://cdrdevapp16.ryanco.com/Juliett/');
+      break;
+      case 'Kilo': I.amOnPage('https://dwsapp16a.ryanco.com/Kilo/');
+      break;
+      case 'Lima': I.amOnPage('https://dwsapp16a.ryanco.com/Lima/');
       break;
 
     }
+ },
 
- }
- 
-    
-  
 
+// This function checks if default password is already reset are not
+   ActualPassword(alert,Password) {
+    let message = alert
+    switch(message) {
+      case 'User ID/Password is invalid. Please try again.' :
+        I.fillField(this.locators.password, Password);
+        I.click('Next');
+        break;
+    }
+},
+
+// This function checks for must change password page
+ MustChange(PageTitle) {
+    let title = PageTitle
+    switch(title) {
+
+      case 'Tracker®PRO - Select Organization' : 
+      {
+        I.see('Select Organization');
+        break;
+      }
+
+      case 'Tracker®PRO - Login' :
+        {
+          I.see('You must change your password to continue');
+          I.fillField('#ctl00_cphBody_txtNewPassword','Password');
+          I.fillField('#ctl00_cphBody_txtVerifyPassword','Password');
+          I.click('Login');
+          I.see('Select Organization');
+          break;
+        }
+      
+    }
+
+
+ },
+
+
+
+// This function let us decide the Org Name and Org Page appearing
+  OrgPage(PageTitle,orgName) {
+    let title = PageTitle
+    switch(title) {
+
+      case 'Tracker®PRO - Select Organization' : 
+      {
+        I.see('Select Organization');
+        I.click("//div[contains(text(),'" + orgName + "')]");
+        I.click(this.locators.selectButton);
+        break;
+      }
+
+      case 'Tracker®PRO - Home' :
+        {
+          break;
+          
+        }
+      
+    }
+ },    
 }
--- specs/test_login.js
+++ specs/test_login.js
@@ -2,21 +2,32 @@
 Feature('TrackerPRO');
 
 let I_login = require('C:/Users/RC08508/CodeceptJS/pages/Loginlocators.js');
+let defaultPassword = 'password';
 
-Scenario('Test Login', (I) => {
+Scenario('Test Login', async (I) => {
 
 //Login
-    I_login.SelectBuild('Charlie');
+    // I.amOnPage('about:blank');   // input custom url
+    I_login.SelectBuild('Hotel');      // input Build Name
     I.see('Ryan');
-    I_login.Username('suryateja.davuluri@ryan.com');
+    I_login.Username('suryateja.davuluri@ryan.com');    // input Username
     I.click('Next');
     I.see('Forgot your password?');
-    I_login.Password('Password');
+    I_login.Password(defaultPassword);
     I.click('Next');
-    I.see('Select Organization');
-    I_login.Org('QA - Surya');
+
+    let alert = await I.grabTextFrom(I_login.locators.alertContent);
+    I_login.ActualPassword(alert,'Password');   // input Password
+
+    let title = await I.grabTitle();
+    I_login.MustChange(title);    
+
+    let page = await I.grabTitle();
+    I_login.OrgPage(page,'QA - Surya');     // input Org Name
+
     I.waitForText('Home','20');
     I.see('Home');
+    
 
 
 });
